910d03d53b43a1fc44b2ff124f6050dd7ebb96e9
Tobby Yuan <yuanzhihang1@126.com>
2016-10-12 16:39:31 +0800

    Update auto_set.py
    Auto init the setting. Get both the port and passwd.

--- auto_set.py
+++ auto_set.py
@@ -4,27 +4,51 @@ import urllib
 import re
 import os
 
-u=urllib.urlopen('http://www.ishadowsocks.net/')
+server_name='usa.iss.tf'
+url='http://www.ishadowsocks.net/'
+code='utf-8'
+
+u=urllib.urlopen(url)
 
 lines=u.readlines()
 def find_pass(lines):
     ok=0
+    passwd=0
+    port=0
     for line in lines:
-        if not re.search(u'us1.iss.tf', line) and ok==0:
+        if not re.search(server_name, line) and ok==0:
             continue
         ok=1
-        if re.search('密码',line):
+        if '端口' in line:
             rst=re.findall(':(\d*)',line)
-            print rst[0]
-            return rst[0]
-    return 0
-passwd=find_pass(lines)
+            print('port:%s'%rst[0])
+            port=rst[0]
+        if '密码' in line:
+            rst=re.findall(':(\d*)',line)
+            print('passwd:%s'%rst[0])
+            passwd=rst[0]
+        if passwd and port:
+            return passwd,port
+    return 0,0
+passwd,port=find_pass(lines)
 assert passwd!=0 ,'cannot get passwd'
 f=open('gui-config.json').read()
 data=json.loads(f)
+
+is_init=0
 for i in data['configs']:
-    if i['server']=='US1.ISS.TF':
+    if i['server']==server_name:
+        is_init=1
         i['password']=passwd
+        i['server_port']=port
+
+if not is_init:
+    new_conf={'server':server_name,
+            'server_port':int(port),
+            'password':passwd,
+            'method':"aes-256-cfb",
+            }
+    data['configs'].append(new_conf)
 s=json.dumps(data)
 f=open('gui-config.json','w')
 f.write(s)
