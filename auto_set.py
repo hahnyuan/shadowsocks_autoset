a348ccc5bec1498aa9ad2b6a5167627ecfc08bcc
Tobby Yuan <yuanzhihang1@126.com>
2016-11-02 19:12:39 +0800

    Update auto_set.py
    FIX the bug of blank passwd

--- auto_set.py
+++ auto_set.py
@@ -22,17 +22,26 @@ def find_pass(lines):
             continue
         if host_name == '':
             host_name = re.findall(':([\w\.]*)<', line)[0]
-            print("host name:%s" % host_name)
+            
         ok = 1
         if '端口' in line:
             rst = re.findall(':(\d*)', line)
-            print('port:%s' % rst[0])
+            
             port = rst[0]
         if '密码' in line:
             rst = re.findall(':(\d*)', line)
-            print('passwd:%s' % rst[0])
+            if len(rst)==0:
+                print("WARNING:the passwd \"%s\" is not set, please don't use it!"%host_name)
+                ok = 0
+                passwd = 0
+                port = 0
+                host_name = ''
+                continue
             passwd = rst[0]
         if passwd and port:
+            print("\nhost name:%s" % host_name)
+            print('port:%s' % rst[0])
+            print('passwd:%s' % rst[0])
             result_list.append((host_name, passwd, port))
             ok = 0
             passwd=0
