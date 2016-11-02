daab35385bd35d8aa82348266297893cce368fb5
Tobby Yuan <yuanzhihang1@126.com>
2016-11-02 16:26:46 +0800

    get all service address

--- auto_set.py
+++ auto_set.py
@@ -4,56 +4,61 @@ import urllib
 import re
 import os
 
-server_name='A服务器'
-url='http://www.ishadowsocks.net/'
-code='utf-8'
+server_name = '服务器地址:'
+url = 'http://www.ishadowsocks.net/'
+code = 'utf-8'
 
-u=urllib.urlopen(url)
+u = urllib.urlopen(url)
+lines = u.readlines()
 
-lines=u.readlines()
 def find_pass(lines):
-    ok=0
-    passwd=0
-    port=0
-    host_name=''
+    result_list = []
+    ok = 0
+    passwd = 0
+    port = 0
+    host_name = ''
     for line in lines:
-        if not re.search(server_name, line) and ok==0:
+        if not re.search(server_name, line) and ok == 0:
             continue
-        if host_name=='':
-            host_name=re.findall(':([\w\.]*)<',line)[0]
-        ok=1
+        if host_name == '':
+            host_name = re.findall(':([\w\.]*)<', line)[0]
+            print("host name:%s" % host_name)
+        ok = 1
         if '端口' in line:
-            rst=re.findall(':(\d*)',line)
-            print('port:%s'%rst[0])
-            port=rst[0]
+            rst = re.findall(':(\d*)', line)
+            print('port:%s' % rst[0])
+            port = rst[0]
         if '密码' in line:
-            rst=re.findall(':(\d*)',line)
-            print('passwd:%s'%rst[0])
-            passwd=rst[0]
+            rst = re.findall(':(\d*)', line)
+            print('passwd:%s' % rst[0])
+            passwd = rst[0]
         if passwd and port:
-            return host_name,passwd,port
-    return 0,0
-host_name,passwd,port=find_pass(lines)
-assert passwd!=0 ,'cannot get passwd'
-f=open('gui-config.json').read()
-data=json.loads(f)
+            result_list.append((host_name, passwd, port))
+            ok = 0
+            host_name = ''
+    return result_list
 
-is_init=0
-for i in data['configs']:
-    if i['server']==host_name:
-        is_init=1
-        i['password']=passwd
-        i['server_port']=port
+result_list = find_pass(lines)
+assert len(result_list) != 0, 'cannot get config'
+f = open('gui-config.json').read()
+data = json.loads(f)
+for host_name, passwd, port in result_list:
+    is_init = 0
+    for i in data['configs']:
+        if i['server'] == host_name:
+            is_init = 1
+            i['password'] = passwd
+            i['server_port'] = port
 
-if not is_init:
-    new_conf={'server':host_name,
-            'server_port':int(port),
-            'password':passwd,
-            'method':"aes-256-cfb",
-            }
-    data['configs'].append(new_conf)
-s=json.dumps(data)
-f=open('gui-config.json','w')
+    if not is_init:
+        new_conf = {'server': host_name,
+                    'server_port': int(port),
+                    'password': passwd,
+                    'method': "aes-256-cfb",
+                    }
+        data['configs'].append(new_conf)
+s = json.dumps(data,indent=3)
+f = open('gui-config.json', 'w')
 f.write(s)
 f.close()
-os.system('Shadowsocks.exe')
+os.systemtem('Shadowsocks.exe')
